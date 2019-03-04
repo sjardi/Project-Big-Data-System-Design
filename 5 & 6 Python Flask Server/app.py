ea4f63f2586c84097a6e9029386e6a3327b2da4f
JonathanTonckensHU <jonathan.tonckens@student.hu.nl>
2019-03-04 12:50:58 +0100

    Added mock python server

---
+++ 5 & 6 Python Flask Server/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/search/<keyword>')
+def search(keyword):
+    return '{"results": [{"title": "Test article 1", "url": "https://google.com", "description":"Welcome to this test article. In this test article I will talk about..."}]}'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
