3406b3b04ac483be3089abd71b399e30adf632f5
JonathanTonckensHU <jonathan.tonckens@student.hu.nl>
2019-03-05 13:38:54 +0100

    Added start of kafka

--- 2&3-Kafka/start-kafka.sh
+++ 2&3-Kafka/start-kafka.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+#all servers are started in the background with &
+#to make it possible to open multiple servers
+
 #start ZooKeeper
-kafka_2.11-2.1.0/bin/zookeeper-server-start.sh kafka_2.11-2.1.0/config/zookeeper.properties
+echo "Starting ZooKeeper..."
+kafka_2.11-2.1.0/bin/zookeeper-server-start.sh kafka_2.11-2.1.0/config/zookeeper.properties &
 #start broker
+echo "Starting Kafka Broker..."
 kafka_2.11-2.1.0/bin/kafka-server-start.sh kafka_2.11-2.1.0/config/server.properties
